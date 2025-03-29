2c577bfff7d090667dcadb99424d927185f3a9aa
SantinoMaza <ing.omarsantos@gmail.com>
2025-03-29 10:04:25 -0600

    update file

---
+++ Python/hanoi.py
@@ -0,0 +1,10 @@
+NUMBER_OF_DISKS = 3
+number_of_moves = 2**NUMBER_OF_DISKS - 1
+print(number_of_moves)
+rods = {
+    'A': list(range(NUMBER_OF_DISKS, 0, -1)),
+    'B': [],
+    'C': []
+}
+def move():
+    pass
