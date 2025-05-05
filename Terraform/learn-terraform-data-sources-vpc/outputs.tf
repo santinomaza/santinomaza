14b0621c796949159f632cd00db01b10dfd57d07
SantinoMaza <ing.omarsantos@gmail.com>
2025-05-05 15:22:15 -0600

    np tf data sources

---
+++ Terraform/learn-terraform-data-sources-vpc/outputs.tf
@@ -0,0 +1,22 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+output "lb_security_group_ids" {
+  description = "Security group IDs for load balancer"
+  value       = [module.lb_security_group.security_group_id]
+}
+
+output "app_security_group_ids" {
+  description = "Security group IDs for application servers"
+  value       = [module.app_security_group.security_group_id]
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnets
+}
