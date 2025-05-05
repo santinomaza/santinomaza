7c60c0df93aaa430d830b299b414965cb7a23223
SantinoMaza <ing.omarsantos@gmail.com>
2025-05-05 15:17:42 -0600

    update tf outputs

--- Terraform/learn-terraform-outputs/terraform.tf
+++ Terraform/learn-terraform-outputs/terraform.tf
@@ -2,15 +2,14 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-  /* Uncomment this block to use Terraform Cloud for this tutorial
+  #Uncomment this block to use Terraform Cloud for this tutorial
 	cloud {
-			organization = "organization-name"
+			organization = "hashicorps-codecamp"
 			workspaces {
 				name = "learn-terraform-outputs"
 			}
 	}
-	*/
-
+	
   required_providers {
     aws = {
       source  = "hashicorp/aws"
