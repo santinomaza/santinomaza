92d7f32f5521bf6a89c320b0b1e1be71fe7b5e76
SantinoMaza <ing.omarsantos@gmail.com>
2025-05-06 15:49:20 -0600

    final tf code org

--- Terraform/learn-terraform-code-organization/main.tf
+++ Terraform/learn-terraform-code-organization/main.tf
@@ -6,165 +6,64 @@ provider "aws" {
 }
 
 resource "random_pet" "petname" {
-  length    = 3
+  length    = 4
   separator = "-"
 }
 
-resource "aws_s3_bucket" "dev" {
-  bucket = "${var.dev_prefix}-${random_pet.petname.id}"
-
+resource "aws_s3_bucket" "bucket" {
+  bucket        = "${var.prefix}-${random_pet.petname.id}"
   force_destroy = true
 }
 
-resource "aws_s3_bucket_website_configuration" "dev" {
-  bucket = aws_s3_bucket.dev.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
+resource "aws_s3_bucket_website_configuration" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  index_document { suffix = "index.html" }
+  error_document { key = "error.html" }
 }
 
-resource "aws_s3_bucket_ownership_controls" "dev" {
-  bucket = aws_s3_bucket.dev.id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
+resource "aws_s3_bucket_ownership_controls" "bucket" {
+  # Must be created BEFORE public access block
+  bucket = aws_s3_bucket.bucket.id
+  rule { object_ownership = "BucketOwnerEnforced" }
 }
 
-resource "aws_s3_bucket_public_access_block" "dev" {
-  bucket = aws_s3_bucket.dev.id
-
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.bucket]
+  
+  bucket = aws_s3_bucket.bucket.id
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "dev" {
+resource "aws_s3_bucket_policy" "bucket" {
   depends_on = [
-    aws_s3_bucket_ownership_controls.dev,
-    aws_s3_bucket_public_access_block.dev,
+    aws_s3_bucket_public_access_block.bucket
   ]
-
-  bucket = aws_s3_bucket.dev.id
-
-  acl = "public-read"
+  
+  bucket = aws_s3_bucket.bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Sid       = "PublicReadGetObject",
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = "s3:GetObject",
+      Resource  = "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
+    }]
+  })
 }
 
-resource "aws_s3_bucket_policy" "dev" {
-  depends_on = [
-    aws_s3_bucket_acl.dev
-  ]
-
-  bucket = aws_s3_bucket.dev.id
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${aws_s3_bucket.dev.id}/*"
-            ]
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_s3_object" "dev" {
+resource "aws_s3_object" "webapp" {
+  depends_on = [aws_s3_bucket_policy.bucket]
+  
+  bucket       = aws_s3_bucket.bucket.id
   key          = "index.html"
-  bucket       = aws_s3_bucket.dev.id
   content      = file("${path.module}/assets/index.html")
   content_type = "text/html"
-}
-
-resource "aws_s3_bucket" "prod" {
-  bucket = "${var.prod_prefix}-${random_pet.petname.id}"
-
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_website_configuration" "prod" {
-  bucket = aws_s3_bucket.prod.id
-
-  index_document {
-    suffix = "index.html"
+  
+  metadata = {
+    cache-control = "max-age=3600"
   }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
-resource "aws_s3_bucket_ownership_controls" "prod" {
-  bucket = aws_s3_bucket.prod.id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "prod" {
-  bucket = aws_s3_bucket.prod.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-
-resource "aws_s3_bucket_acl" "prod" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.prod,
-    aws_s3_bucket_public_access_block.prod,
-  ]
-
-  bucket = aws_s3_bucket.prod.id
-
-  acl = "public-read"
-}
-
-resource "aws_s3_bucket_policy" "prod" {
-  depends_on = [
-    aws_s3_bucket_acl.prod
-  ]
-
-  bucket = aws_s3_bucket.prod.id
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${aws_s3_bucket.prod.id}/*"
-            ]
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_s3_object" "prod" {
-  key          = "index.html"
-  bucket       = aws_s3_bucket.prod.id
-  content      = file("${path.module}/assets/index.html")
-  content_type = "text/html"
 }
--- Terraform/learn-terraform-code-organization/variables.tf
+++ Terraform/learn-terraform-code-organization/variables.tf
@@ -2,13 +2,16 @@
 # SPDX-License-Identifier: MPL-2.0
 
 variable "region" {
-  description = "This is the cloud hosting region where your webapp will be deployed."
+  description = "AWS region"
+  type        = string
 }
 
-variable "dev_prefix" {
+#variable "dev_prefix" {
+variable "prefix" {
   description = "This is the environment where your webapp is deployed. qa, prod, or dev"
+  type        = string
 }
 
-variable "prod_prefix" {
-  description = "This is the environment where your webapp is deployed. qa, prod, or dev"
-}
+#variable "prod_prefix" {
+#  description = "This is the environment where your webapp is deployed. qa, prod, or dev"
+#}
