d26824f23093420a502b6715f422f5d7135d9435
SantinoMaza <ing.omarsantos@gmail.com>
2025-05-06 10:35:19 -0600

    final tf functions

--- Terraform/learn-terraform-functions/main.tf
+++ Terraform/learn-terraform-functions/main.tf
@@ -76,11 +76,38 @@ resource "aws_security_group" "sg_8080" {
 
 
 resource "aws_instance" "web" {
-  ami                         = data.aws_ami.ubuntu.id
+  #ami                         = data.aws_ami.ubuntu.id
+  ami                         = lookup(var.aws_amis, var.aws_region)
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.subnet_public.id
-  vpc_security_group_ids      = [aws_security_group.sg_8080.id]
+  #vpc_security_group_ids      = [aws_security_group.sg_8080.id]
+  vpc_security_group_ids      = [aws_security_group.sg_22.id, aws_security_group.sg_8080.id]
+
   associate_public_ip_address = true
   user_data                   = templatefile("user_data.tftpl", { department = var.user_department, name = var.user_name })
+  key_name                    = aws_key_pair.ssh_key.key_name
 
 }
+
+#ssh-keygen -C "your_email@example.com" -f ssh_key
+
+resource "aws_security_group" "sg_22" {
+  name = "sg_22"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port = 22
+    to_port  = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name = "ssh_key"
+  public_key = file("ssh_key.pub")
+}
+
+#ssh ubuntu@$(terraform output -raw web_public_ip) -i ssh_key
+
+
--- Terraform/learn-terraform-functions/variables.tf
+++ Terraform/learn-terraform-functions/variables.tf
@@ -30,3 +30,13 @@ variable "user_department" {
   description = "The organization the user belongs to: dev, prod, qa"
   default     = "learn"
 }
+
+variable "aws_amis" {
+  type = map
+  default = {
+    "us-east-1" = "ami-04b70fa74e45c3917"
+    "us-west-2" = "ami-08012c0a9ee8e21c4"
+    "us-east-2" = "ami-09040d770ffe2224f"
+  }
+}
+
