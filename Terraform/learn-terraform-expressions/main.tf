6820ba596b800f96f92376b67a69150f70dee11c
SantinoMaza <ing.omarsantos@gmail.com>
2025-05-06 10:45:36 -0600

    upgrade tf functions files

--- Terraform/learn-terraform-expressions/main.tf
+++ Terraform/learn-terraform-expressions/main.tf
@@ -88,3 +88,17 @@ resource "aws_instance" "ubuntu" {
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.subnet_public.id
 }
+
+resource "random_id" "id" {
+  byte_length = 8
+}
+
+locals {
+  name  = (var.name != "" ? var.name : random_id.id.hex)
+  owner = var.team
+  common_tags = {
+    Owner = local.owner
+    Name  = local.name
+  }
+}
+
