2ebfe5781eeeb0899dfc5fe172031e8dd6d011c4
SantinoMaza <ing.omarsantos@gmail.com>
2025-05-06 12:35:33 -0600

    final tf modules use

--- Terraform/learn-terraform-modules-use/main.tf
+++ Terraform/learn-terraform-modules-use/main.tf
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MPL-2.0
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-2"
 
   default_tags {
     tags = {
@@ -34,7 +34,7 @@ module "ec2_instances" {
 
   name = "my-ec2-cluster"
 
-  ami                    = "ami-0c5204531f799e0c6"
+  ami                    = "ami-0fb653ca2d3203ac1"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [module.vpc.default_security_group_id]
   subnet_id              = module.vpc.public_subnets[0]
--- Terraform/learn-terraform-modules-use/terraform.tf
+++ Terraform/learn-terraform-modules-use/terraform.tf
@@ -2,20 +2,19 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-  /* Uncomment this block to use Terraform Cloud for this tutorial
+  #Uncomment this block to use Terraform Cloud for this tutorial
   cloud {
-    organization = "organization-name"
+    organization = "hashicorps-codecamp"
     workspaces {
       name = "learn-terraform-module-use"
     }
   }
-  */
-
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.49.0"
     }
   }
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.5.7"
 }
