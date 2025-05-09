2bcd1b013a9993630f0046f74b347b18019a1ffd
SantinoMaza <ing.omarsantos@gmail.com>
2025-05-09 10:17:16 -0600

    no tf troublesh

---
+++ Terraform/learn-terraform-troubleshooting/outputs.tf
@@ -0,0 +1,17 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.web_app.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.web_app.public_ip
+}
+
+output "instance_name" {
+  description = "Tags of the EC2 instance"
+  value       = aws_instance.web_app.tags
+}
